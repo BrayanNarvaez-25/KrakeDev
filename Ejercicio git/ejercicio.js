70f2958a054f0b4f88c7a6d4fde505389f9d9c62
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-16 17:27:22 -0500

    el boton modifica la imagen

--- Ejercicio git/ejercicio.js
+++ Ejercicio git/ejercicio.js
@@ -1,3 +1,5 @@
 saludar = function(){
-    console.log("ingresa a saludar")
+    let cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src = "./Imagenes/minion.gif";
+    console.log("ingresa a saludar");
 }
