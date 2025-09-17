e33a1042ce13b9b9f42dc76ece9ec7561f69709e
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-17 16:18:42 -0500

    Función mostrar imagen completa

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -5,6 +5,7 @@ saludar = function(){
     let estatura = recuperarFloat("txtES");
     let mensajeBienvenida = "Bienvenido" + " " + nombre + " " + apellido;
     mostrarTexto("lblResultado",mensajeBienvenida);
+    mostrarImagen("imgSaludo","./imagenes/saludo.gif");
 }
 
 recuperarTexto = function(idComponente){
@@ -29,3 +30,9 @@ mostrarTexto= function(idComponente,mensaje){
     componente = document.getElementById(idComponente);
     componente.innerText = mensaje;
 }   
+
+mostrarImagen = function(idComponente,rutaImagen){
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.src = rutaImagen;
+}
