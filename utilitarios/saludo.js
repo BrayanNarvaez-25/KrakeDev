a40ee898f803afc72fff0ff3078f1a8cebf4eab9
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-17 15:00:33 -0500

    Se imprentan las funciones recuperarInt y recuperarFloat

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,11 +1,24 @@
 saludar = function(){
     let nombre = recuperarTexto("txt");
     let apellido = recuperarTexto("txtA");
+    let edad =  recuperarInt("txtE");
+    let estatura = recuperarFloat("txtES");
 }
+
 recuperarTexto = function(idComponente){
     let cmp;
     let valorIngresado;
     cmp = document.getElementById(idComponente);
     valorIngresado = cmp.value;
     return valorIngresado;
+}
+recuperarInt = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
 }
