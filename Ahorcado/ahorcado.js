80b371e6a6c97c3b8f03106c8e87dcb6b3030171
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-27 18:12:26 -0500

    Paso 1 completado

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -1,8 +1,23 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta = "";
+
 esMayuscula = function (caracter) {
     let esMayuscula = caracter.charCodeAt(0);
     if (esMayuscula >= 65 && esMayuscula <= 90) {
         return true;
     }
     return false
+}
+
+guardarPalabra = function(){
+    let palabra = recuperarTexto("txtSecreta");
+    if(palabra.length != 5){
+        alert("DEBE INGRESAR UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYUSCULAS");
+    }
+    for(let i = 0;i < palabra.length;i++){
+        if(!esMayuscula(palabra[i]))
+        alert("DEBE INGRESAR SOLO LETRAS MAYUSCULAS");
+    }
+    palabraSecreta = palabra;
+    console.log(palabraSecreta)
 }
