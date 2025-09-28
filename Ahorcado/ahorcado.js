45a3f4498e1fead5487d90c5be26815babc414c3
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-27 23:12:05 -0500

    Paso 6 completado y corregido errores

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -12,65 +12,100 @@ esMayuscula = function (caracter) {
     return false
 }
 
-guardarPalabra = function(){
+guardarPalabra = function () {
     let palabra = recuperarTexto("txtSecreta");
-    if(palabra.length != 5){
+    if (palabra.length != 5) {
         alert("DEBE INGRESAR UNA PALABRA DE EXACTAMENTE 5 LETRAS MAYUSCULAS");
     }
-    for(let i = 0;i < palabra.length;i++){
-        if(!esMayuscula(palabra[i]))
-        alert("DEBE INGRESAR SOLO LETRAS MAYUSCULAS");
+    for (let i = 0; i < palabra.length; i++) {
+        if (!esMayuscula(palabra[i]))
+            alert("DEBE INGRESAR SOLO LETRAS MAYUSCULAS");
     }
     palabraSecreta = palabra;
     console.log(palabraSecreta)
 }
 
-mostrarLetra = function(letra,posicion){
-    let posicionLetra = posicion.charAt(0)
-    if(posicionLetra.length == 0){
-        mostrarTexto("div0",letra);
+mostrarLetra = function (letra, posicion) {
+    let posicionLetra = posicion;
+    if (posicionLetra == 0) {
+        mostrarTexto("div0", letra);
     }
-    if(posicionLetra.length == 1){
-        mostrarTexto("div1",letra);
+    if (posicionLetra == 1) {
+        mostrarTexto("div1", letra);
     }
-    if(posicionLetra.length == 2){
-        mostrarTexto("div2",letra);
+    if (posicionLetra == 2) {
+        mostrarTexto("div2", letra);
     }
-    if(posicionLetra.length == 3){
-        mostrarTexto("div3",letra);
+    if (posicionLetra == 3) {
+        mostrarTexto("div3", letra);
     }
-    if(posicionLetra.length == 5){
-        mostrarTexto("div5",letra);
+    if (posicionLetra == 4) {
+        mostrarTexto("div4", letra);
     }
 }
 
-validar = function(letra){
+validar = function (letra) {
     let letrasEncontradas = 0;
     let posicion;
-    for(let i = 0;i < letra.length;i++){
-        posicion = letra.charAt(i);
-        if(posicion == letra){
-            mostrarLetra(letra,posicion);
+    for (let i = 0; i <= palabraSecreta.length; i++) {
+        posicion = palabraSecreta.charAt(i);
+        if (letra == posicion) {
+            mostrarLetra(letra, i);
             letrasEncontradas++;
+            concidencias++;
         }
     }
-   errores++;
-    alert("LA LETRA NO ES PARTE DE LA PALABRA")
+    if (letrasEncontradas == 0) {
+        errores++;
+        mostrarAhorcado();
+        alert("LA LETRA NO ES PARTE DE LA PALABRA")
+    }
+
 }
 
-ingresarLetra = function(){
+ingresarLetra = function () {
+    intentos++;
     let letra = recuperarTexto("txtLetra");
-    if(letra == esMayuscula(letra)){
+    if (esMayuscula(letra)) {
         validar(letra);
-        intentos++;
-        concidencias++;
-        if(concidencias == 5){
+        if (concidencias == 5) {
             alert("HA GANADO");
         }
-        if(intentos == 10){
+        if (intentos == 10) {
             alert("HA PERDIDO");
         }
-    }else(
-        alert("SOLO SE ACEPTAN LETRAS MAYUSCULAS")
-    )
+    } else {
+        errores++;
+        alert("SOLO SE ACEPTAN LETRAS MAYUSCULAS");
+    }
+}
+
+mostrarAhorcado = function(){
+    if(errores == 1){
+        mostrarImagen("ahorcadoImagen","Ahorcado_01.png")
+    }
+     if(errores == 2){
+        mostrarImagen("ahorcadoImagen","Ahorcado_02.png")
+    }
+     if(errores == 3){
+        mostrarImagen("ahorcadoImagen","Ahorcado_03.png")
+    }
+     if(errores == 4){
+        mostrarImagen("ahorcadoImagen","Ahorcado_04.png")
+    }
+     if(errores == 5){
+        mostrarImagen("ahorcadoImagen","Ahorcado_05.png")
+    }
+     if(errores == 6){
+        mostrarImagen("ahorcadoImagen","Ahorcado_06.png")
+    }
+     if(errores == 7){
+        mostrarImagen("ahorcadoImagen","Ahorcado_07.png")
+    }
+     if(errores == 8){
+        mostrarImagen("ahorcadoImagen","Ahorcado_08.png")
+    }
+     if(errores == 9){
+        mostrarImagen("ahorcadoImagen","Ahorcado_09.png")
+    }
 }
