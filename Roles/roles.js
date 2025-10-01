da5035dba5cc5cf7afde9e6fead4be892e381311
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-30 19:45:25 -0500

    Se agrega funcion mostrarEmpleados que se invoca desde la funcion mostrarOpcionEmpleados

--- Roles/roles.js
+++ Roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
-    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1727392035", nombre: "Brayan", apellido: "Narvaez", sueldo: 800.0}
 ]
 
 mostrarOpcionEmpleado = function () {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol = function () {
@@ -15,9 +17,25 @@ mostrarOpcionRol = function () {
     ocultarComponente("divResumen");
 }
 
-mostrarOpcionResumen = function(){
+mostrarOpcionResumen = function () {
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
+}
+
+mostrarEmpleados = function () {
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table><tr>" + "<th> CEDULA </th>" +
+        "<th> NOMBRE </th>" + "<th> APELLIDO </th>" + "<th> SUELDO </th>" + "</tr>";
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        contenidoTabla += "<tr> <td> " + elementoEmpleado.cedula + " </td>" +
+            "<td> " + elementoEmpleado.nombre + " </td>" +
+            "<td> " + elementoEmpleado.apellido + " </td>" + 
+            "<td> " + elementoEmpleado.sueldo + " </td>" +"</tr>";
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
 
